d987c9558dd55e56e1fec09a59ed8843bc08948c
Jack Kellenberger <jackson@kellenberger.io>
2022-07-12 15:27:19 -0400

    Refactor Dice class into module

--- dice.rb
+++ dice.rb
@@ -1,32 +1,20 @@
 # frozen-string-literal: true
 
-# Generic Dice roller class
-class Dice
-  attr_reader :quantity, :sides
-
-  def initialize(args)
-    args = defaults.merge(args)
-    @quantity = args[:quantity]
-    @sides = args[:sides]
-  end
-
-  def defaults
-    { quantity: 2, sides: 6 }
-  end
-
-  def roll
+# Dice roller for 2d6 games
+module Dice
+  def self.roll(quantity = 2, sides = 6)
     Array.new(quantity) { rand(1..sides) }
   end
 
-  def sum
+  def self.sum
     roll.reduce(0) { |sum, die| sum + die }
   end
 
-  def check(target)
+  def self.check(target)
     sum >= target
   end
 
-  def throw(target)
+  def self.throw(target)
     sum <= target
   end
 end
